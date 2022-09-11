f99f4a4f468113bc48c5be2b48165f73a578435b
IgorAlshuk <igordilar@mail.ru>
2022-09-12 02:36:21 +0300

    fead(favorites): add new page and components

---
+++ src/components/Heard/Heard.tsx
@@ -0,0 +1,11 @@
+import SvgSelectors from "../../assets/SvgSelectors";
+import { StyledHeard } from "./styles";
+
+
+export const Heard = () => {
+  return (
+    <StyledHeard >
+      <SvgSelectors id="heard" />
+    </StyledHeard>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { App } from "./App";
+import { store } from "./store/store";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const app = document.getElementById("root");
+if (!app) {
+  throw new Error("No root element found");
+}
+const root = createRoot(app);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
