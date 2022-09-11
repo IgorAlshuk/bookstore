ce58a7b914714334ec50d720a7dde955030aff79
IgorAlshuk <igordilar@mail.ru>
2022-09-12 01:27:27 +0300

    feat(main): add styles

--- src/components/MainTemplate/styles.ts
+++ src/components/MainTemplate/styles.ts
@@ -1,8 +1,14 @@
 import styled from "styled-components";
 import { media } from "../../ui/media";
 
+export const StyledApp = styled.div`
+  max-width: 1920px;
+  width: 100%;
+`;
+
 export const Container = styled.div`
   max-width: 1120px;
+  padding: 0 16px;
   width: 100%;
   margin: 0 auto;
   ${media.tablet} {
