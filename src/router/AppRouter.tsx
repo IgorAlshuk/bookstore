baf6c63ae58e9e12ff31ddee1c42a4cdceaaefac
IgorAlshuk <igordilar@mail.ru>
2022-09-12 01:45:47 +0300

    feat(home page): add adaptive

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,11 +4,10 @@ import { BookDetails } from "../pages/BookDetails";
 import { Home } from "../pages/Home";
 import { SearchBooks } from "../pages/SearchBooks";
 import { routes } from "../router/routes";
-
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainTemplate />}>
+      <Route path={routes.HOME} element={<MainTemplate />}>
         <Route path={routes.HOME} element={<Home />} />
         <Route path={routes.DETAILS} element={<BookDetails />} />
         <Route path={routes.SEARCH} element={<SearchBooks />} />
