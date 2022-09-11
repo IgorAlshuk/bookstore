1b241ba88939c5e61bac02f9d63a0679dffa9e03
IgorAlshuk <igordilar@mail.ru>
2022-09-11 23:45:48 +0300

    feat(template): add components

--- src/components/MainTemplate.tsx
+++ src/components/MainTemplate.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Subscribe from "./Subscribe/Subscribe";
 
 const MainTemplate = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Subscribe />
+      <Footer />
     </>
   );
 };
