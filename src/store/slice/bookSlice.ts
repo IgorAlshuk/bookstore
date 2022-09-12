f12554a13934add4082fbb9b2077d41019fe17cc
IgorAlshuk <igordilar@mail.ru>
2022-09-12 16:17:53 +0300

    feat(bookslice): add type AsyncThunk

--- src/store/slice/bookSlice.ts
+++ src/store/slice/bookSlice.ts
@@ -1,20 +1,100 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { IBook } from "../types";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { bookApi } from "../../services/bookService";
+import { IDetailsBookApi, INewBooksApi } from "../../services/types";
+import { IBookApi, IArguments, IresultBooksApi } from "../types";
 
-const initialState: IBook[] = [
-  {
-    image: "https://itbook.store/img/books/9781642002133.png",
-    isbn13: "9781642002133",
-    price: "$0.00",
+const initialState: IBookApi = {
+  books: [],
+  error: null,
+  status: "idle",
+  total: "0",
+  result: {
+    authors: "",
+    desc: "",
+    error: "",
+    image: "",
+    isbn10: "",
+    isbn13: "",
+    language: "",
+    pages: "",
+    pdf: {},
+    price: "",
+    publisher: "",
+    rating: "",
     subtitle: "",
-    title: "Azure Pipelines Succinctly",
-    url: "https://itbook.store/books/9781642002133",
+    title: "",
+    url: "",
+    year: "",
   },
-];
+};
+
+export const fetchBook = createAsyncThunk<INewBooksApi>(
+  "book/fetchBook",
+  async () => {
+    const newBooks = await bookApi.getNewBooks();
+    return newBooks;
+  }
+);
+export const fetchBookDetails = createAsyncThunk<IDetailsBookApi, string>(
+  "books/fetchBookDetails",
+  async (id) => {
+    const bookDetails = await bookApi.getBookDetails(id);
+    return bookDetails;
+  }
+);
+
+export const fetchBooksBySearch = createAsyncThunk<IresultBooksApi, IArguments>(
+  "books/fetchBooksBySearch",
+  async ({ title, page }) => {
+    const resultBooks = await bookApi.getBooksBySearch(title, page);
+    return resultBooks;
+  }
+);
 
 const bookSlice = createSlice({
   name: "book",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchBook.pending, (state) => {
+      state.status = "loading";
+      state.error = null;
+    });
+    builder.addCase(fetchBook.fulfilled, (state, { payload }) => {
+      state.status = "success";
+      state.books = payload.books;
+      state.error = payload.error;
+      state.total = payload.total;
+    });
+    builder.addCase(fetchBook.rejected, (state, { payload }) => {
+      state.status = "error";
+      state.error = payload;
+    });
+    builder.addCase(fetchBooksBySearch.pending, (state) => {
+      state.status = "loading";
+      state.error = null;
+    });
+    builder.addCase(fetchBooksBySearch.fulfilled, (state, action) => {
+      state.books = action.payload.books;
+      state.status = "success";
+      state.total = action.payload.total;
+    });
+    builder.addCase(fetchBooksBySearch.rejected, (state, action) => {
+      state.error = action.error;
+      state.status = "error";
+    });
+    builder.addCase(fetchBookDetails.pending, (state) => {
+      state.status = "loading";
+      state.error = null;
+    });
+    builder.addCase(fetchBookDetails.fulfilled, (state, action) => {
+      state.result = action.payload;
+      state.status = "success";
+    });
+    builder.addCase(fetchBookDetails.rejected, (state) => {
+      state.status = "loading";
+      state.error = null;
+    });
+  },
 });
 export default bookSlice.reducer;
